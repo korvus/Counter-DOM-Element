9be75386c351050a1373f1708c01e931f687a503
simon <ertel.simon+github@gmail.com>
2015-08-17 11:29:56 +0200

    Add the abilities to detects spacer
    check within the src attributes of all pic name including.

--- dce/actions/countSpacer.js
+++ dce/actions/countSpacer.js
@@ -1,2 +1,12 @@
-var spacer = document.getElementsByClassName("spacer");
-alert(spacer.length+" .spacer element in this page.");
+var spacerClass = document.getElementsByClassName("spacer");
+var allPics = document.getElementsByTagName('img');
+
+var a, b = 0;
+for(a=0;a<allPics.length;a++){
+	var srcAttr = allPics[a].src;
+	if(srcAttr.search(/spacer/gi)!=-1){
+		b++;
+	}
+}
+
+alert(spacerClass.length+" .spacer element in this page.\n and "+b+" pictures including 'spacer' in it name.");
