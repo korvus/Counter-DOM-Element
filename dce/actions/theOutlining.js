b190893e8665f2b94bd1cc09d6a580bdd86b0a5a
simon <ertel.simon+github@gmail.com>
2015-08-21 18:17:24 +0200

    Add more precisions about table elements
    include tr, td, th, thead, tbody, tfoot, caption and colgroup in table tag colors.

--- dce/actions/theOutlining.js
+++ dce/actions/theOutlining.js
@@ -11,6 +11,7 @@ if(myElements){
 
   //https://html.spec.whatwg.org/multipage/indices.html#element-content-categories
   var childrenPhrasing = ['abbr','b','bdi','bdo','button','cite','code','data','dfn','em','h1','h2','h3','h4','h5','h6','i','kbd','label','legend','mark','meter','output','p','pre','progress','q','rp','rt','ruby','s','samp','small','span','strong','sub','summary','sup','time','u','var'];
+  var tableElts = ['table','tr','td','th','thead','tbody','tfoot','caption','colgroup'];
 
   var color = "0f65da";
   for (var i = 0; i < myElements.length; i++) {
@@ -24,7 +25,7 @@ if(myElements){
     */
     if(childrenPhrasing.indexOf(myElements[i].nodeName.toLowerCase()) != -1){
       color = "ceda0f";
-    }else if(myElements[i].nodeName.toLowerCase()=="table"){
+    }else if(tableElts.indexOf(myElements[i].nodeName.toLowerCase()) > -1){
       color = "E03926";
     }else{
       color = "0f65da";
